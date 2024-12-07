4a9b27ac5de3549c22fc09ed0fe1f8542ba5a9bc
X1nDi <arseniy2018ars@gmail.com>
2024-12-07 18:56:50 +0500

    friends, menu, progress bar, cases

--- static/script.js
+++ static/script.js
@@ -19,5 +19,37 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 });
 
-// Полоса прогресса
-let currentScore = 0;
+ // Получаем элементы
+    const menuIcon = document.getElementById('menu-icon');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    // Переключение видимости меню при клике на картинку
+    menuIcon.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('active');
+    });
+
+    // Закрываем меню, если кликнули вне его
+    document.addEventListener('click', (event) => {
+        if (!menuIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove('active');
+        }
+    });
+
+  // Получаем элементы
+        const clickButton = document.getElementById('clickButton');
+        const progressBar = document.getElementById('progress-bar');
+
+        // Событие клика на изображение
+        clickButton.addEventListener('click', () => {
+            let currentValue = progressBar.value; // Текущее значение прогресса
+
+            // Увеличиваем значение полосы на 10% при каждом клике, но не больше 100%
+            if (currentValue < 100) {
+                progressBar.value = currentValue + 10;
+            }
+
+            // Проверяем, достиг ли прогресс максимума
+            if (progressBar.value === 100) {
+                alert('Прогресс завершён!');
+            }
+        });
